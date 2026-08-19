8abca0247ae5a3e363417b8234710b3290b774e8
Drashti <drashti1712@gmail.com>
2026-08-19 20:20:27 +0530

    Fixed build issue in SimilarMovies

--- src/components/movieDetails/SimilarMovies.tsx
+++ src/components/movieDetails/SimilarMovies.tsx
@@ -1,25 +1,33 @@
-import { useEffect, useState } from "react";
+import { useCallback } from "react";
 import Gallery from "../Gallery/Gallery";
-import type { Movie } from "../../types";
+import type { MovieResponse } from "../../types";
 import { getSimilarMovies } from "../../services/movieApi";
 import { useParams } from "react-router";
+import useFetch from "../../hooks/useFetch";
 
 const SimilarMovies = () => {
-  const [similarMovies, setSimilarMovies] = useState<Movie[]>([]);
   const { id } = useParams();
 
-  useEffect(() => {
-    (async () => {
-      if (!id) return;
-      const response = await getSimilarMovies(id);
-      setSimilarMovies(response.results);
-    })();
+  const fetchSimilarMovies = useCallback(() => {
+    if (!id) {
+      throw new Error("Movie ID is missing!");
+    }
+    return getSimilarMovies(id);
   }, [id]);
+
+  const {
+    data: similarMovies,
+    loading,
+    error,
+  } = useFetch<MovieResponse>(fetchSimilarMovies, true);
+
   return (
     <div>
       <Gallery
-        data={similarMovies}
+        data={similarMovies?.results ?? []}
         heading="You might also like:"
+        loading={loading}
+        error={error}
         type="carousel"
       />
     </div>
